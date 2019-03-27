49cc5782614bb780abf89263dab2e9b6d1f462bb
vitekzkytek <7482296+vitekzkytek@users.noreply.github.com>
2019-03-27 15:43:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-def reply():
-  return 'Je nám líto, ale k vašim informacím se v současnosti nemůžeme vyjadřovat.'
+def reply(lang='cs'):
+  
+  d = {
+    'cs':'Je nám líto, ale k vašim informacím se v současnosti nemůžeme vyjadřovat.'
+    'en':'We are sorry, but we cannot provide any comments in this matter.'
+  }
+  return d[lang]
